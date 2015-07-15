7203725ff09c19d2f4562ddbf412ec6c0b10b41d
root <david.bennett@percona.com>
2015-07-15 03:02:19 +0000

    Added Mongo C++ Driver to build script.  The build script will now build in-place

--- scripts/build_MetricBench.sh
+++ scripts/build_MetricBench.sh
@@ -13,94 +13,146 @@
 #
 # Currently supports Ubuntu 12 / 14
 
+# NOTE:  This script is designed to run as root
+# in a docker container or bare bones VM
+
 # david.bennett at percona.com - 6/9/2015
 
-# Note: you will need to run as root for the first section
-
 BUILD_STATIC=0
-BUILD_ROOT=${HOME}/build
 
-# --- root section ---
+# determine if we are running from inside of
+# the MetricBench repository or we are running
+# as a free standing script.
+
+SCRIPTPATH=$(cd $(dirname "$0"); pwd)
+PARENTPATH=$(cd "${SCRIPTPATH}/.."; pwd)
+GITCNF="${PARENTPATH}/.git/config"
+
+if [ -f "${GITCNF}" ] && grep -Fq MetricBench "${GITCNF}"; then
+  BUILD_ROOT="${PARENTPATH}/build"
+  INREPO=1
+else
+  BUILD_ROOT="${SCRIPTPATH}/build"
+  INREPO=0
+fi
 
 # install package dependencies for building
 
-apt-get update
+if [ ! -d "${BUILD_ROOT}" ]; then
 
-HAS_GPP_48=$(apt-cache search 'g\+\+-4\.8' | head -n1 | awk '{print $1}')
-
-if [ -z "${HAS_GPP_48}" ]; then
-  apt-get install -y --force-yes python-software-properties
-  add-apt-repository -y ppa:ubuntu-toolchain-r/test
   apt-get update
+
+  HAS_GPP_48=$(apt-cache search 'g\+\+-4\.8' | head -n1 | awk '{print $1}')
+
+  if [ -z "${HAS_GPP_48}" ]; then
+    apt-get install -y --force-yes python-software-properties
+    add-apt-repository -y ppa:ubuntu-toolchain-r/test
+    apt-get update
+  fi
+
+  apt-get install -y --force-yes \
+    vim \
+    bzr git wget \
+    gcc-4.8 g++-4.8 \
+    cmake make \
+    libmysqlclient18 libmysqlclient-dev \
+    libbz2-dev
+
+  # make gcc-4.8 the default
+
+  cd /usr/bin
+  for CMD in cpp gcc g++; do
+    [ -L ${CMD} ] && rm ${CMD}
+    ln -s ${CMD}-4.8 ${CMD}
+  done 
+
+  mkdir ${BUILD_ROOT}
+
 fi
-
-apt-get install -y --force-yes \
-  vim \
-  bzr git wget \
-  gcc-4.8 g++-4.8 \
-  cmake make \
-  libmysqlclient18 libmysqlclient-dev \
-  libbz2-dev
-
-# make gcc-4.8 the default
-
-cd /usr/bin
-for CMD in cpp gcc g++; do
-  [ -L ${CMD} ] && rm ${CMD}
-  ln -s ${CMD}-4.8 ${CMD}
-done 
-
-# --- user section (does not require root) ---
 
 # download and install Boost 1.58
 
-mkdir ${BUILD_ROOT}
 cd ${BUILD_ROOT}
-wget -q -O - \
+
+if [ ! -d "boost_1_58_0" ]; then
+  wget -q -O - \
         http://sf.net/projects/boost/files/boost/1.58.0/boost_1_58_0.tar.gz/download \
         | tar xzvf -
+fi
 cd boost_1_58_0
 BOOST_ROOT=${BUILD_ROOT}/MetricBench_boost
-mkdir -p ${BOOST_ROOT}
-./bootstrap.sh --prefix=${BOOST_ROOT} \
-        --libdir=${BOOST_ROOT}/lib --includedir=${BOOST_ROOT}/include \
-        --with-libraries=program_options,filesystem,system,test,thread,regex
-if [ "${BUILD_STATIC}" -gt 0 ]; then
-  ./b2 --build-type=complete --layout=tagged link=static install | tee b2.out
-else
-  ./b2 --build-type=complete --layout=tagged link=shared install | tee b2.out
+if [ ! -e "${BOOST_ROOT}/lib/libboost_exception.a" ]; then
+  mkdir -p ${BOOST_ROOT}
+  ./bootstrap.sh --prefix=${BOOST_ROOT} \
+          --libdir=${BOOST_ROOT}/lib --includedir=${BOOST_ROOT}/include \
+          --with-libraries=program_options,filesystem,system,test,thread,regex
+  if [ "${BUILD_STATIC}" -gt 0 ]; then
+    ./b2 --build-type=complete --layout=tagged link=static install | tee b2.out
+  else
+    ./b2 --build-type=complete --layout=tagged link=shared install | tee b2.out
+  fi
 fi
 export BOOST_ROOT
 
 # MySQL Connector C++ - 1.1.5
 
 cd ${BUILD_ROOT}
-bzr branch lp:~mysql/mysql-connector-cpp/trunk mysql-connector-cpp
+if [ ! -d "${BUILD_ROOT}/mysql-connector-cpp" ]; then
+  bzr branch lp:~mysql/mysql-connector-cpp/trunk mysql-connector-cpp
+fi
 cd mysql-connector-cpp
 bzr revert -r1.1.5
 MYSQLCONNECTORCPP_ROOT_DIR=${BUILD_ROOT}/MetricBench_mysqlcpp
-cmake -DMYSQLCLIENT_STATIC_BINDING:BOOL=1 -DCMAKE_INSTALL_PREFIX:PATH=${MYSQLCONNECTORCPP_ROOT_DIR} .
-make install
+if [ "$(find ${BUILD_ROOT}/MetricBench_mysqlcpp/ \( -name '*.a' -or -name '*.so' \) | wc -l)" == "0" ]; then
+  cmake -DMYSQLCLIENT_STATIC_BINDING:BOOL=1 -DCMAKE_INSTALL_PREFIX:PATH=${MYSQLCONNECTORCPP_ROOT_DIR} .
+  make install
+fi
 export MYSQLCONNECTORCPP_ROOT_DIR
+
+# Mongo C++ driver
+
+cd ${BUILD_ROOT}
+if [ ! -d "${BUILD_ROOT}/mongo-cxx-driver" ]; then
+  git clone https://github.com/mongodb/mongo-cxx-driver.git
+fi
+cd mongo-cxx-driver
+git checkout legacy
+MONGO_LIB_ROOT_DIR=${BUILD_ROOT}/MetricBench_mongo-cxx-driver
+if [ "$(find ${MONGO_LIB_ROOT_DIR} \( -name '*.a' -or -name '*.so' \) | wc -l)" ]; then 
+  if [ "${BUILD_STATIC}" -gt 0 ]; then
+    scons --prefix=${MONGO_LIB_ROOT_DIR} --extrapath=${BOOST_ROOT} --dynamic-boost=off install
+  else
+    scons --prefix=${MONGO_LIB_ROOT_DIR} --extrapath=${BOOST_ROOT} --dynamic-boost=on install
+  fi
+fi
+export MONGO_LIB_ROOT_DIR
 
 # download and build Metric Bench using exported roots of boost, mysqlcppconn
 
-cd ${BUILD_ROOT}
-git clone https://github.com/Percona-Lab/MetricBench.git
-cd MetricBench/src
+if [ "${INREPO}" -eq "1" ]; then
+  MBROOT="${PARENTPATH}"
+else
+  cd ${BUILD_ROOT}
+  if [ ! -d "${BUILD_ROOT}/MetricBench" ]; then
+    git clone https://github.com/Percona-Lab/MetricBench.git
+  fi
+  MBROOT="${BUILD_ROOT}/MetricBench"
+fi
+cd ${MBROOT}/src
 ./clean.sh all
 if [ "${BUILD_STATIC}" -gt 0 ]; then
   cmake -DBUILD_STATIC=1 -DMYSQLCONNECTORCPP_ROOT_DIR:PATH=${MYSQLCONNECTORCPP_ROOT_DIR} .
 else
-  cmake -DMYSQLCONNECTORCPP_ROOT_DIR:PATH=${MYSQLCONNECTORCPP_ROOT_DIR} .
+  cmake -DMYSQLCONNECTORCPP_ROOT_DIR:PATH=${MYSQLCONNECTORCPP_ROOT_DIR} \
+        -DMONGO_LIB_ROOT_DIR:PATH=${MONGO_LIB_ROOT_DIR}  .
 fi
 make
 
 # Epilogue 
 
 echo "-----"
-if [ -e $BUILD_ROOT/MetricBench/src/MetricBench ]; then
-  echo "Build was successful: ${BUILD_ROOT}/MetricBench/src/MetricBench"
+if [ -e $MBROOT/src/MetricBench ]; then
+  echo "Build was successful: ${MBROOT}/src/MetricBench"
 else
   echo "Build was unsuccessful"
 fi
